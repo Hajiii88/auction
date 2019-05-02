40755f38966b3d21f4f8f8bbb07d35f7927389d0
Hajiii88 <kessaku1022@icloud.com>
2019-05-02 15:53:34 +0900

    最終コミット！！ おつかれ 削除 追加シヤした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,16 +13,20 @@ class ItemsController < ApplicationController
     @item.save
     redirect_to @item
   end
-
   def edit
     @item = Item.find(params[:id])
   end
-
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
     redirect_to @item
   end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+  end
+
   private
 
   def item_params
